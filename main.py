a2f3158c81484ffe10d9a5493031c8bc603c14a9
cirinoval <cirvalentin1@gmail.com>
2025-04-08 19:56:21 +0100

    added the first program

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+import tmpdeviation
 def interface():
     print("=" * 80)
     print("|"+ " "*78 + "|")
     print("|"+ " "*26 + "Temperature Change Dataset" + " "*26 + "|")
     print("|"+ " "*78 + "|")
     print("=" * 80)
-    choice=str(input("\n\nWhich program would you like to access? \n<1> \n<2> \n<3> \n<4> \n<5> \n<6> \n"))
+    choice=str(input("\n\nWhich program would you like to access? \n<1>Temperature Peaks \n<2> \n<3> \n<4> \n<5> \n"))
     if choice=="1":
-        print("Program 1")
+        tmpdeviation.tmpdev()
     elif choice=="2":
         print("Program 2")
     elif choice=="3":
@@ -15,8 +16,6 @@ def interface():
         print("Program 4")
     elif choice=="5":
         print("Program 5")
-    elif choice=="6":
-        print("Program 6")
     else: #in case the user inputs the wrong value
         print("Incorrect Value, restarting: \n")
 interface()
