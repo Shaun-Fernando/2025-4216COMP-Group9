3da6ed6ff22523e2f909b2a726ea9ab50bfea176
harrysheard <harrysheard2004@gmail.com>
2025-04-18 14:27:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import TemperatureChangeCompareDaniel
 import temperaturedeviation
 import TemperaturePerYear
 import CompareTwoCountriesOverFourYears
+import Tempanomolyscomparedtoaverage
+import longtermaverage
 def interface():
     print("=" * 80)
     print("|"+ " "*78 + "|")
     print("|"+ " "*26 + "Temperature Change Dataset" + " "*26 + "|")
     print("|"+ " "*78 + "|")
     print("=" * 80)
-    choice=str(input("\n\nWhich program would you like to access? \n<1>Temperature Peaks \n<2>Temperature Comparisons \n<3>Four Year Comparison \n<4>Temperature Per Year \n<5>Cumulative Temperature change for Continents \n<6>Basic Area Comparing\n"))
+    choice=str(input("\n\nWhich program would you like to access? \n<1>Temperature Peaks \n<2>Temperature Comparisons \n<3>Four Year Comparison \n<4>Temperature Per Year \n<5>Cumulative Temperature change for Continents \n<6>Basic Area Comparing\n<7>temperature anomolies\n<8>longterm averages"))
     if choice=="1":
         temperaturedeviation.tempdev()
     elif choice=="2":
@@ -21,6 +23,10 @@ def interface():
         cumulativeContinents.cumulativeContinents()
     elif choice=="6":
         search.searchCompare()
+    elif choice=="7":
+        Tempanomolyscomparedtoaverage.temp_anomalies_lowest()
+    elif choice=="8":
+        longtermaverage.avg_temp_by_year()
     
     else: #in case the user inputs the wrong value
         print("Incorrect Value, restarting: \n")#
